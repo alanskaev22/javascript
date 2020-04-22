9480e9083a5f54f165abfa3b69284d6f09efbc9b
Alan Skaev <askaev@rccl.com>
2020-04-21 20:09:52 -0400

    Vars

--- app/js/index.js
+++ app/js/index.js
@@ -6,3 +6,24 @@ import { log, logTitle } from 'logger';
 /* your imports */
 logTitle('Title')
 /* coding examples */
+
+
+var person = {
+    name: "Alan",
+    age: 44,
+    hasDriverLicense: true,
+    dob:"12/12/1222",
+    address:{
+        street: "123Main",
+        zipCode: "02030",
+        country: "USA"
+    }
+};
+
+log(JSON.stringify(person));
+
+log(JSON.stringify(person.name));
+log(JSON.stringify(person.age));
+log(JSON.stringify(person.hasDriverLicense));
+
+log(Object.values(person));
